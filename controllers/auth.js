14e2cf317f3ca95f344a61e06b2e2c066d347bd3
andresmotta9 <46831406+andresmotta9@users.noreply.github.com>
2025-04-11 09:22:30 -0400

    Added login against db

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,15 +36,40 @@ const createUser = async (req, res = response) => {
   }
 };
 
-const loginUser = (req, res = response) => {
+const loginUser = async (req, res = response) => {
   const { email, password } = req.body;
 
-  return res.json({
-    ok: true,
-    msg: 'login',
-    email,
-    password,
-  });
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: `The user with that email does not exists `,
+      });
+    }
+
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: `Password Incorrect`,
+      });
+    }
+
+    res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Please talk with the server's administartor",
+    });
+  }
 };
 
 const renewToken = (req, res = response) => {
