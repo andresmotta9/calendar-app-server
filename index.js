f4ca017ce74be65cea33d4f746c84884665425cb
andresmotta9 <46831406+andresmotta9@users.noreply.github.com>
2025-04-11 09:16:56 -0400

    Created connection with mongoose

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
 // Initialize server
 const app = express();
+
+// Database
+dbConnection();
 
 // Public Directory
 app.use(express.static('public'));
