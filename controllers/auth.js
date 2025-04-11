eab60b102ac31538355caf1dfc4343f00e6d62e2
andresmotta9 <46831406+andresmotta9@users.noreply.github.com>
2025-04-11 09:17:16 -0400

    Inserted user to db with the pwd encrypted

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,39 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 
-const createUser = (req, res = response) => {
+const createUser = async (req, res = response) => {
   const { name, email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
 
-  res.status(201).json({
-    ok: true,
-    msg: 'new',
-    name,
-    email,
-    password,
-  });
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: `The user with the email ${email} already exists `,
+      });
+    }
+
+    user = new User(req.body);
+
+    //Encrypt Password
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
+    await user.save();
+
+    res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Please talk with the server's administartor",
+    });
+  }
 };
 
 const loginUser = (req, res = response) => {
